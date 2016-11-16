eea066e7cdeae096d7523a806b58143019354c02
tache <chris@mondocoolstudios.com>
2016-11-16 14:17:26 -0500

    Adding descriptive note

--- lib/active_record/acts/shopping_cart/collection.rb
+++ lib/active_record/acts/shopping_cart/collection.rb
@@ -5,7 +5,7 @@ module ActiveRecord
         #
         # Adds a product to the cart
         #
-        def add(object, price, quantity = 1, cumulative = true)
+        def add(object, price, quantity = 1, note ="", cumulative = true)
           cart_item = item_for(object)
 
           if cart_item
@@ -14,7 +14,7 @@ module ActiveRecord
             cart_item.save
             cart_item
           else
-            shopping_cart_items.create(item: object, price: price, quantity: quantity)
+            shopping_cart_items.create(item: object, price: price, quantity: quantity, :note => note)
           end
         end
 
--- lib/acts_as_shopping_cart/schema.rb
+++ lib/acts_as_shopping_cart/schema.rb
@@ -8,6 +8,7 @@ module ActsAsShoppingCart
       integer :quantity       # Holds the quantity of the object
       integer :item_id        # Holds the object id
       string  :item_type      # Holds the type of the object, for polymorphism
+      string  :note           # Holds a descriptive note for the object
       integer :price_cents, default: 0, null: false  # Holds the price of the item
       string  :price_currency, default: "USD", null: false # Holds the currency for the price
     end
